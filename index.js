c271fd281381d67e49606e70e3712b1aca7c1a6a
sarvajeet-hue <sarvajeet.1923en1095@kiet.edu>
2024-09-16 17:05:14 +0530

    First INit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+app.post('/remove-background', async (req, res) => {
+  try {
+    const response = await axios.post('https://api.lightxeditor.com/external/api/v1/remove-background', req.body, {
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': '1bdb846f1ab84fa98163fb15ec784f26_2db605f0fb574a7dbf726bce5dbfe952_andoraitools',
+      },
+    });
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.post('/remove-background-with-orderId' , async (req , res) => {
+    try{
+        const {orderId} = req.body
+        const response = await axios.post('https://api.lightxeditor.com/external/api/v1/order-status' , {orderId} ,{
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': '1bdb846f1ab84fa98163fb15ec784f26_2db605f0fb574a7dbf726bce5dbfe952_andoraitools',
+              },
+    })
+         res.send(response.data);
+    }catch(error){
+        res.status(500).send(error.message);
+    }
+})
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});

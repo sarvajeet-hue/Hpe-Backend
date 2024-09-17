354c48b150722406dff2636c1771437b8c3d46e8
sarvajeet-hue <sarvajeet.1923en1095@kiet.edu>
2024-09-17 16:37:28 +0530

    updated code

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(express.json())
 
 app.post('/remove-background', async (req, res) => {
   try {
+    
     const response = await axios.post('https://api.lightxeditor.com/external/api/v1/remove-background', req.body, {
       headers: {
         'Content-Type': 'application/json',
@@ -22,8 +23,7 @@ app.post('/remove-background', async (req, res) => {
 
 app.post('/remove-background-with-orderId' , async (req , res) => {
     try{
-        const {orderId} = req.body
-        const response = await axios.post('https://api.lightxeditor.com/external/api/v1/order-status' , {orderId} ,{
+            const response = await axios.post('https://api.lightxeditor.com/external/api/v1/order-status' , req.body ,{
             headers: {
                 'Content-Type': 'application/json',
                 'x-api-key': '1bdb846f1ab84fa98163fb15ec784f26_2db605f0fb574a7dbf726bce5dbfe952_andoraitools',
@@ -33,6 +33,22 @@ app.post('/remove-background-with-orderId' , async (req , res) => {
     }catch(error){
         res.status(500).send(error.message);
     }
+})
+
+
+app.post('/avtar' , async (req , res) => {
+  try{
+      console.log(req.body)
+      const response = await axios.post('https://api.lightxeditor.com/external/api/v1/avatar' , req.body ,{
+          headers: {
+              'Content-Type': 'application/json',
+              'x-api-key': '1bdb846f1ab84fa98163fb15ec784f26_2db605f0fb574a7dbf726bce5dbfe952_andoraitools',
+            },
+  })
+       res.send(response.data);
+  }catch(error){
+      res.status(500).send(error.message);
+  }
 })
 
 app.listen(5000, () => {

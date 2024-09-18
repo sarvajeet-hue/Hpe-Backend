8dfa176fdd2e03d33c101d93138385137c9d57ae
sarvajeet-hue <sarvajeet.1923en1095@kiet.edu>
2024-09-18 15:06:05 +0530

    Updated code

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const cors = require('cors');
 const app = express();
 
+
 app.use(cors());
 app.use(express.json())
 
@@ -12,7 +13,7 @@ app.post('/remove-background', async (req, res) => {
     const response = await axios.post('https://api.lightxeditor.com/external/api/v1/remove-background', req.body, {
       headers: {
         'Content-Type': 'application/json',
-        'x-api-key': '1bdb846f1ab84fa98163fb15ec784f26_2db605f0fb574a7dbf726bce5dbfe952_andoraitools',
+        'x-api-key': "61b833e3775949919410374d2b830b36_7081146179a447db9a79a65842a74dc7_andoraitools",
       },
     });
     res.send(response.data);
@@ -26,7 +27,7 @@ app.post('/remove-background-with-orderId' , async (req , res) => {
             const response = await axios.post('https://api.lightxeditor.com/external/api/v1/order-status' , req.body ,{
             headers: {
                 'Content-Type': 'application/json',
-                'x-api-key': '1bdb846f1ab84fa98163fb15ec784f26_2db605f0fb574a7dbf726bce5dbfe952_andoraitools',
+                'x-api-key': "61b833e3775949919410374d2b830b36_7081146179a447db9a79a65842a74dc7_andoraitools",
               },
     })
          res.send(response.data);
@@ -42,12 +43,29 @@ app.post('/avtar' , async (req , res) => {
       const response = await axios.post('https://api.lightxeditor.com/external/api/v1/avatar' , req.body ,{
           headers: {
               'Content-Type': 'application/json',
-              'x-api-key': '1bdb846f1ab84fa98163fb15ec784f26_2db605f0fb574a7dbf726bce5dbfe952_andoraitools',
+              'x-api-key': "61b833e3775949919410374d2b830b36_7081146179a447db9a79a65842a74dc7_andoraitools",
             },
   })
+  console.log("response:" , response)
        res.send(response.data);
   }catch(error){
       res.status(500).send(error.message);
+  }
+})
+
+
+app.post('/avtar-status' , async (req , res) => {
+  console.log(req.body)
+  try{
+    const response = await axios.post('https://api.lightxeditor.com/external/api/v1/order-status' , req.body ,{
+      headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': "61b833e3775949919410374d2b830b36_7081146179a447db9a79a65842a74dc7_andoraitools",
+        },
+    })
+    res.send(response?.data)
+  }catch(error){
+    res.status(500).send(error.message);
   }
 })
 
